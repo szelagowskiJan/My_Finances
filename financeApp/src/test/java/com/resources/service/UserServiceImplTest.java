fcdef8e33321ea6b2ea9bd9976592e298ef1cc61
Janek <szelagowski.jan1@gmail.com>
2023-08-17 20:44:09 +0200

    Adding unit tests for UserServiceImpl

--- financeApp/src/test/java/com/resources/service/UserServiceImplTest.java
+++ financeApp/src/test/java/com/resources/service/UserServiceImplTest.java
@@ -1,15 +1,22 @@
 package com.resources.service;
 
+import com.resources.dto.UserDto;
 import com.resources.entity.UserEntity;
 import com.resources.repository.RoleRepository;
 import com.resources.repository.UserRepository;
 import com.resources.service.impl.UserServiceImpl;
+import com.resources.service.serviceConfig.ServiceConfig;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.same;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -18,7 +25,7 @@ import org.springframework.mail.javamail.JavaMailSender;
  *
  * @author Praca
  */
-public class UserServiceImplTest {
+public class UserServiceImplTest extends ServiceConfig {
 
     @Mock
     private UserRepository userRepository;
@@ -88,5 +95,61 @@ public class UserServiceImplTest {
 
         UserEntity fullName = userService.findUserEntityByEmail("@wp.pl");
         assertEquals(null, fullName.getEmail());
+    }
+
+    @Test
+    public void findAllUsers_correctUserEntity_correctUserDto() {
+
+        List<UserEntity> listUserEntity = new ArrayList<>();
+        listUserEntity.add(getCorrectUserEntity());
+
+        when(userRepository.findAll()).thenReturn(listUserEntity);
+        when(roleRepository.findById(1)).thenReturn(getUserRole());
+
+        List<UserDto> users = userService.findAllUsers();
+        Assert.assertNotNull(users);
+    }
+
+    @Test
+    public void changeUserData_correctUserEntity_UpdateNameAndLastName() {
+        UserEntity userEntity = getCorrectUserEntity();
+
+        when(userRepository.findByEmail(userEntity.getEmail())).thenReturn(userEntity);
+        userService.changeUserData(userEntity);
+
+        verify(userRepository).save(same(userEntity));
+    }
+
+    @Test
+    public void changeUserData_UserEntityWithoutFirstNameAndLastName_dontUpdateNameAndLastName() {
+        UserEntity userEntityFromDb = getUserEntityWithoutFirstNameAndLastName();
+        UserEntity correctUserEntity = getCorrectUserEntity();
+
+        when(userRepository.findByEmail(correctUserEntity.getEmail())).thenReturn(correctUserEntity);
+        userService.changeUserData(userEntityFromDb);
+
+        verify(userRepository).save(same(correctUserEntity));
+    }
+
+    @Test
+    public void changeUserData_UserEntityEmptyFirstNameAndLastName_dontUpdateNameAndLastName() {
+        UserEntity userEntityFromDb = getUserEntityEmptyFirstNameAndLastName();
+        UserEntity correctUserEntity = getCorrectUserEntity();
+
+        when(userRepository.findByEmail(correctUserEntity.getEmail())).thenReturn(correctUserEntity);
+        userService.changeUserData(userEntityFromDb);
+
+        verify(userRepository).save(same(correctUserEntity));
+    }
+
+    @Test
+    public void saveUserEntity_correctUserEntity_correctSave() {
+        UserDto userDto = getCorrectUserDto();
+        UserEntity userEntity = getCorrectUserEntity();
+
+        when(userRepository.save(userEntity)).thenReturn(userEntity);
+        UserEntity userEntity1 = userService.saveUserEntity(userDto);
+
+        Assert.assertEquals(userEntity, userEntity1);
     }
 }
