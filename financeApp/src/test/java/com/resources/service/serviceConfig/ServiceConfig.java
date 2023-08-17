e3fd094e73ab17820493a044dd702e005311c17c
Janek <szelagowski.jan1@gmail.com>
2023-08-17 20:45:32 +0200

    Adding a helper class for service unit tests

---
+++ financeApp/src/test/java/com/resources/service/serviceConfig/ServiceConfig.java
@@ -0,0 +1,63 @@
+package com.resources.service.serviceConfig;
+
+import com.resources.dto.UserDto;
+import com.resources.entity.Role;
+import com.resources.entity.UserEntity;
+
+/**
+ *
+ * @author Praca
+ */
+public class ServiceConfig {
+
+    protected UserEntity getCorrectUserEntity() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(5);
+        userEntity.setFirstName("Adam");
+        userEntity.setLastName("Nowak");
+        userEntity.setEmail("nowak@wp.pl");
+        userEntity.setPassword("4vgsd31s2131g");
+        userEntity.setUserRole(1);
+        userEntity.setEnabled(false);
+        return userEntity;
+    }
+
+    protected UserEntity getUserEntityWithoutFirstNameAndLastName() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(5);
+        userEntity.setEmail("nowak@wp.pl");
+        userEntity.setUserRole(1);
+        userEntity.setEnabled(false);
+        return userEntity;
+    }
+
+    protected UserEntity getUserEntityEmptyFirstNameAndLastName() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(5);
+        userEntity.setFirstName("");
+        userEntity.setLastName("");
+        userEntity.setEmail("nowak@wp.pl");
+        userEntity.setUserRole(1);
+        userEntity.setEnabled(false);
+        return userEntity;
+    }
+
+    protected UserDto getCorrectUserDto() {
+        UserDto userDto = new UserDto();
+        userDto.setUserId(5);
+        userDto.setFirstName("Adam");
+        userDto.setLastName("Nowak");
+        userDto.setEmail("nowak@wp.pl");
+        userDto.setPassword("4vgsd31s2131g");
+        userDto.setUserRole(1);
+        userDto.setEnabled(false);
+        return userDto;
+    }
+
+    protected Role getUserRole() {
+        Role role = new Role();
+        role.setId(1);
+        role.setRoleName("USER");
+        return role;
+    }
+}
